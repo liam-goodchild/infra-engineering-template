913e32b3814584b2b77e3cc49bdf444e59c6169f
Liam <liamgoodchild12@hotmail.co.uk>
2026-01-27 16:29:37 +0000

    Referencing tags

--- infra/resources.tf
+++ infra/resources.tf
@@ -3,5 +3,6 @@ resource "null_resource" "placeholder" {
     prefix       = local.prefix
     prefix_short = local.prefix_short
     st_naming    = jsonencode(local.st_naming)
+    tags         = jsonencode(local.tags)
   }
 }
